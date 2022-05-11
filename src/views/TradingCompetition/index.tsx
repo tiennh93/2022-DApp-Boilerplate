430e1fd7b07650c23baf73f7ac03a94bb49a49f9
TienNguyenHuu <tiennh@vgs.studio>
2022-05-11 18:12:32 +0700

    first commit

---
+++ src/views/TradingCompetition/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import MoDCompetition from './MoDCompetition';
+
+const TradingCompetitionPage: React.FC = () => {
+  // TODO: Add <SubMenu /> add historical pages
+  return <MoDCompetition />;
+};
+
+export default TradingCompetitionPage;
